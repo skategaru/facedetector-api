de0f069ed30e2e1484922238daeb6ba223a5ccde
skategaru <sreekanth414@gmail.com>
2021-12-24 22:44:04 +0530

    adding port from env

--- server.js
+++ server.js
@@ -32,6 +32,6 @@ app.get('/profile/:id', (req, res) => profile.handleProfile(req, res, db));
 app.put('/image', (req, res) => image.handleImage(req, res, db));
 app.post('/imageUrl', (req, res) => image.handleApiCall(req, res));
 
-app.listen(3100, () => {
-  console.log("app is running on port 3100");
+app.listen(process.env.PORT || 3100, () => {
+  console.log(`app is running on port ${process.env.PORT}`);
 });
